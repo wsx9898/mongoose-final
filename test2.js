7f69668515dc87681bd78807fa4ce8cd248760f0
wsx9898 <wsx9898@gmail.com>
2024-08-25 14:10:49 +0800

    this is first commit for mongoose-final

---
+++ test2.js
@@ -0,0 +1,25 @@
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { product: { ...i.productId._doc }, quantity: i.quantity };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart(); // Assuming you have a clearCart method in User model
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+};
